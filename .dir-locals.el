b46c5608a530c19724c2cf04642aa31eabf4b9e6
RangHo Lee <hello@rangho.me>
2023-11-08 21:08:56 +0100

    [feat] Initialize `org-roam` root

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((eval . (let ((knowledge-root (locate-dominating-file default-directory ".dir-locals.el")))
+                  (setq org-roam-directory (concat knowledge-root "src/nodes"))
+                  (setq org-roam-db-location (concat knowledge-root "org-roam.db")))))))
