c3373e93970a4692a138da5a41f457e5773d2576
RangHo Lee <hello@rangho.me>
2025-09-28 17:17:26 +0900

    chore: remove everything except actual notes

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((eval . (let ((knowledge-root (locate-dominating-file default-directory ".dir-locals.el")))
-                  (setq org-roam-directory (concat knowledge-root "src/notes"))
-                  (setq org-roam-db-location (concat knowledge-root "org-roam.db")))))))
--- README.md
+++
@@ -1,8 +0,0 @@
-# RangHo's Online Brain Backup
-
-> "Not everything. I just know what I know." \
-> — Tsubasa Hanekawa, _Monogatari Series_.
-
-Personal Knowledge Base, inspired by [Nikita Voloboev's amazing knowledge wiki][1].
-
-[1]: https://wiki.nikiv.dev/
